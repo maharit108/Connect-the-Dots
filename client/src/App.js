2d99f83d14a31936d3877e69014df18317c4aa3d
mp4 <puleri.mp4@gmail.com>
2021-03-24 10:16:25 -0700

    Added Navbar (without routes)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Login from './components/Login/Login.js'
+import Header from './components/Header/Header.js'
 
 function App() {
   const [user, setUser] = [null]
-  
+
   return (
     <div className="App">
+      <Header />
       {!user ? <Login setUser={setUser} /> : ''}
     </div>
   );
