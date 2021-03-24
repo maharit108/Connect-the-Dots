d2b021b444d52194f817b5df4b018e4560e97921
Han Nguyen <hanvnguyen94@gmail.com>
2021-03-24 14:14:05 -0400

    - added basic form for TutorProfile - created route for Tutors component but its not working yet because we need to define the root route first

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
+import './App.css'
+import React from 'react'
+import { Route } from 'react-router-dom'
+
 
 import Login from './components/Login/Login.js'
 import Header from './components/Header/Header.js'
+import LeftForm from './components/TutorProfile/LeftForm'
 
-function App() {
+function App () {
   const [user, setUser] = [null]
 
   return (
     <div className="App">
       <Header />
       {!user ? <Login setUser={setUser} /> : ''}
+      <Route path='/tutors' user={user} render={() => (
+        <LeftForm />
+      )} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+import App from './App'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
-);
+)
 
