7d6460274714ea98f71df0d59418f686f1f929a4
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2021-03-25 15:33:57 -0500

    added  ternary for signin/signout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,39 +1,45 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
-import './Header.css'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import "./Header.css";
 
-const Header = props => {
+const Header = (props) => {
+  const { user } = props;
   return (
     <Navbar expand="lg" className="Navbar_bg">
       <Navbar.Brand className="logo" href="/">
-      Connect <br /> The Dots
+        Connect <br /> The Dots
       </Navbar.Brand>
-      <Navbar.Toggle expand="md" className="Navbar_Toggle" aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle
+        expand="md"
+        className="Navbar_Toggle"
+        aria-controls="basic-navbar-nav"
+      />
       <Navbar.Collapse className="Nav_group">
         <Button className="Nav_button N1">Find a Tutor</Button>
-        <Nav.Link className="Nav N2">
-        About
+        <Nav.Link className="Nav N2">About</Nav.Link>
+        <Nav.Link className="Nav N3" href="#student-info">
+          Schools
         </Nav.Link>
-        <Nav.Link className="Nav N3" href='#student-info'>
-        Schools
+        <Nav.Link className="Nav N4" href="#tutors">
+          Tutors
         </Nav.Link>
-        <Nav.Link className="Nav N4" href='#tutors'>
-        Tutors
+        <Nav.Link className="Nav N5" href="#change-password">
+          Change Password
         </Nav.Link>
-        <Nav.Link className="Nav N5" href='#change-password'>
-        Change Password
-        </Nav.Link>
-        <Nav.Link className="Nav N6" href='#sign-in'>
-        Sign in
-        </Nav.Link>
-        <Nav.Link className="Nav N7" href='#sign-out'>
-        Sign Out
-        </Nav.Link>
+        {!user ? (
+          <Nav.Link className="Nav N6" href="#sign-in">
+            Sign in
+          </Nav.Link>
+        ) : (
+          <Nav.Link className="Nav N7" href="#sign-out">
+            Sign Out
+          </Nav.Link>
+        )}
       </Navbar.Collapse>
-      </Navbar>
-  )
-}
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
