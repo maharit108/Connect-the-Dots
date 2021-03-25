fa5196f0f1aa14786daaf39db2b863a0b050d83b
mp4 <puleri.mp4@gmail.com>
2021-03-25 09:54:22 -0700

    Header, Footer, and Landing Page Components
    Still need routing

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,24 @@
 import React, {Fragment} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
 import './Header.css'
 
 const Header = props => {
   return (
+<<<<<<< HEAD
 
     <Navbar>
       <Navbar.Brand href='#'>Logo</Navbar.Brand>
       <div className="Nav_group">
+=======
+    <Navbar className="Navbar_bg" expand="md">
+      <Navbar.Brand className="logo" href="/">
+      Connect <br /> The Dots
+      </Navbar.Brand>
+      <Navbar.Toggle className="Navbar_Toggle" aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="Nav_group">
+>>>>>>> 70c1f92... Header, Footer, and Landing Page Components
         <Nav className="Nav">
         About
         </Nav>
@@ -17,6 +27,7 @@ const Header = props => {
         </Nav.Link>
         <Nav.Link className="Nav" href='#tutors'>
         Tutors
+<<<<<<< HEAD
         </Nav.Link>
         <Nav.Link className="Nav" href='#change-password'>
         Change Password
@@ -28,6 +39,14 @@ const Header = props => {
         Sign Out
         </Nav.Link>
       </div>
+=======
+        </Nav>
+        <Button className="Nav_button">Find a Tutor</Button>
+        <Nav className="Nav">
+        Login/Sign in
+        </Nav>
+      </Navbar.Collapse>
+>>>>>>> 70c1f92... Header, Footer, and Landing Page Components
     </Navbar>
   )
 }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import CardColumns from 'react-bootstrap/CardColumns'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import './Home.css'
+
+
+
+
+const Home = () => {
+  return (
+    <div className="landing_parent">
+      <div className="photo_hero">
+        <h1 className="photo_hero_header">Find the best local tutors to <br/>impact your student's success.</h1>
+        <Button className='photo_hero_button'>Find A Tutor</Button>
+      </div>
+      <div className="CardColumns">
+        <div className='cards_background'>
+          <h4 className="card_header">What others said about our services</h4>
+          <CardColumns type="text">
+            <Card className="testomonials">
+              <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </Card>
+            <Card className="testomonials">
+              <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            </Card>
+            <Card className="testomonials">
+              <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Card>
+            <Card className="testomonials">
+              <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </Card>
+            <Card className="testomonials">
+              <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Card>
+          </CardColumns>
+          <div  className="card_button_div">
+            <Button className="card_button">Who We Are</Button>
+          </div>
+        </div>
+      </div>
+      <ListGroup as='div' horizontal='md' className='need_help'>
+        <ListGroup.Item style={{ border: 'none', backgroundColor: '#F5F5F5' }} className='need_help_header'>
+          <h3>Need Help?</h3>
+          <p>
+          For more info, you can contact directly at <br />xxxx@connectthedots.com
+          </p>
+        </ListGroup.Item>
+        <ListGroup.Item style={{ border: 'none', backgroundColor: '#F5F5F5' }}>
+          <Button className='FAQ'>FAQ</Button>
+        </ListGroup.Item>
+      </ListGroup>
+    </div>
+  )
+}
+
+export default Home
