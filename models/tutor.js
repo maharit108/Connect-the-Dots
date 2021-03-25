e528ec5124b396cfd325888b26ed65467fcc4d40
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2021-03-25 13:38:35 -0500

    tutor seed file made

--- models/tutor.js
+++ models/tutor.js
@@ -4,12 +4,8 @@ const Schema = mongoose.Schema;
 const Tutor = new Schema(
   {
     name: { type: String, required: true },
-    avatar: { type: String, required: false },
-    education: { type: String, required: true }, // proof of teaching ability
-    description: { type: String, required: false },
-    backgroundCheck: { type: Boolean, required: true }, // crimianl background check as part of being a sponsored tutor
-    employer: {type: String, required: true}, // small business to possibly be advertiesd and proof tutor is a worker
-    email: {type: String, required: true}, // to make contact
+    photo: { type: String, required: false },
+    brief: { type: String, required: false },
   },
   {
     timestamps: true,
--- seed/tutors.js
+++ seed/tutors.js
@@ -5,11 +5,33 @@ const Tutor = require("../models/tutor");
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const main = async () => {
-  const tutors = [{}];
+  const tutors = [
+    {
+      name: "John Doe",
+      photo:
+        "https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+      brief:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    },
+    {
+      name: "Sally Fields",
+      photo:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
+      brief:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    },
+    {
+      name: "Richard Pryor",
+      photo:
+        "https://images.unsplash.com/photo-1594616838951-c155f8d978a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+      brief:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    },
+  ];
 
   await Tutor.deleteMany();
   await Tutor.insertMany(tutors);
-  conseole.log("Tutors Created");
+  console.log("Tutors Created");
 };
 
 const run = async () => {
