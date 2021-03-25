240600eb687240431dd5ebe6a6c106dae16ac435
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2021-03-25 15:56:19 -0500

    ternary cont.

--- client/src/components/SignOut/SignOut.js
+++ client/src/components/SignOut/SignOut.js
@@ -1,21 +1,20 @@
-import {useEffect} from 'react'
-import { withRouter } from 'react-router-dom'
+import { useEffect } from "react";
+import { withRouter } from "react-router-dom";
 
-import { signOut } from '../../api/auth.js'
-import './SignOut.css'
+import { signOut } from "../../api/auth.js";
+import "./SignOut.css";
 
-function SignOut (props) {
-  const { history, setUser, user } = props
- 
+function SignOut(props) {
+  const { history, setUser, user } = props;
+
   useEffect(() => {
     signOut(user.userData)
-      .then(res => console.log('sign-out'))
+      .then(() => setUser(null))
       .catch(console.error)
-      .finally(() => history.push('/'))
-      .finally(() => setUser(null))
-  })
+      .finally(() => history.push("/"));
+  }, []);
 
-    return ''
+  return "";
 }
 
-export default withRouter(SignOut)
+export default withRouter(SignOut);
