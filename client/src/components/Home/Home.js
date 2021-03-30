3ed89f872c43611aa8ede57e9abca67b5253a985
mp4 <puleri.mp4@gmail.com>
2021-03-29 21:06:10 -0700

    Added placeholder names and name styling on testomonials

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -21,18 +21,23 @@ const Home = () => {
           <CardColumns type="text">
             <Card className="testomonials">
               <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <p className="card_name card_p">-Dude 1</p>
             </Card>
             <Card className="testomonials">
               <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              <p className="card_name card_p">-Dudette 2</p>
             </Card>
             <Card className="testomonials">
               <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p className="card_name card_p">-Dudette 3</p>
             </Card>
             <Card className="testomonials">
               <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className="card_name card_p">-Dude 4</p>
             </Card>
             <Card className="testomonials">
               <p className="card_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="card_name card_p">-Dudette 5</p>
             </Card>
           </CardColumns>
           <div  className="card_button_div">
